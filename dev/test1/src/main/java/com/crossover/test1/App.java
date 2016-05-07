14d87e489d9ca590d77ca13eeafeb519c103166d
Mario de Sa Vera <desavera@gmail.com>
2016-05-07 12:23:53 -0300

    INIT

---
+++ dev/test1/src/main/java/com/crossover/test1/App.java
@@ -0,0 +1,13 @@
+package com.crossover.test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
